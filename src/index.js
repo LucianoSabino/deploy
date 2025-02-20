5582cea8dab2b8cfa6cd12ee8d04be97b9dfbfaf
Luciano Sabino <lb255043@gmail.com>
2025-02-19 22:02:54 -0300

    deploy

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Teste da api");
+});
+
+app.get("/usuario", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(8080, () => {
+  console.log("Api rodado na porta 8080");
+});
