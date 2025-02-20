0f4257a5125539db5b1e99aefff11bf7c9b51b19
Luciano Sabino <lb255043@gmail.com>
2025-02-20 11:57:52 -0300

    test usuario sem banco

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { app } from "./server/server.js";
 
-import { Knex } from "./server/database/Knex/index.js";
+// import { Knex } from "./server/database/Knex/index.js";
 
-console.log("Iniciando migrações...");
-await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados PostgreSQL na nuvem
-console.log("Migrações concluídas. Iniciando o servidor...");
+// console.log("Iniciando migrações...");
+// await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados PostgreSQL na nuvem
+// console.log("Migrações concluídas. Iniciando o servidor...");
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Api rodado na porta 8080");
--- src/server/providers/usuario/Create.js
+++ src/server/providers/usuario/Create.js
@@ -1,13 +1,9 @@
-import { Knex } from "../../database/Knex/index.js";
+// import { Knex } from "../../database/Knex/index.js";
 
 export const create = async (data) => {
   try {
-    const [result] = await Knex("usuarios").insert(data).returning("id");
-    if (typeof result === "object" && result.id) {
-      return result.id;
-    } else if (typeof result === "number") {
-      return result;
-    }
+    const [result] = "Feito";
+    return result;
 
     throw new Error("Erro ao cadastrar o registro"); // Corrigido o erro de digitação
   } catch (error) {
