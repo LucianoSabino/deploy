7e42a82be9a64615079b0b96270d1837f6c04756
Luciano Sabino <lb255043@gmail.com>
2025-02-20 11:27:58 -0300

    teste migração

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { app } from "./server/server.js";
 
+import { Knex } from "./server/database/Knex/index.js";
+
+console.log("Iniciando migrações...");
+await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados PostgreSQL na nuvem
+console.log("Migrações concluídas. Iniciando o servidor...");
+
 app.listen(process.env.PORT || 8080, () => {
   console.log("Api rodado na porta 8080");
 });
