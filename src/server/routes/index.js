c8c2f25eac73c363f4e5523bf4e86aa5fc7fca81
Luciano Sabino <lb255043@gmail.com>
2025-02-19 22:44:03 -0300

    test 7

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
-import { createUp, createValidation } from "../controller/usuario/create.js";
-
+import { singUp, singUpValidation } from "../controller/login/SignUp.js";
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("Teste da api");
 });
 
-// Cadastro de usuario e login
-router.post("/usuario", createValidation, createUp);
-router.post("/sign", singUpValidation, singUp);
+router.post("/login", loginValidation, login);
 
 export { router };
